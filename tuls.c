85cc8ad06190b1916ad54a03a025ab44936c5cbd
matvii0392 <tuo07428@temple.edu>
2024-01-31 22:54:01 -0500

    Completely overhauled the project because I was dumb......

--- tuls.c
+++ tuls.c
@@ -1,71 +1,38 @@
 //
 // Created by Illia Matviienko on 1/24/24.
 //
-#include <dirent.h>
-#include "libc.h"
-#include "tuls.h"
-#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
-int scandirr (void) {
+int main(int argc, char *argv[]) {
+    DIR *dir;
+    struct dirent *entry;
+    int num_files;
     struct dirent **namelist;
-    int n;
-    n = scandir(".", &namelist, NULL, alphasort);
-    if (n == -1) {
-        perror("scandir");
+
+    printf("commandline args: \n\n");
+    for (size_t i = 0; i < argc; i++)
+    {
+        printf("Position %li:%s\n", i, argv[i]);
+    }
+    printf("-----------Current----------\n");
+    //printf("\nValue of argc (argument count) was %d\n", argc);
+
+
+    dir = opendir(".");
+    if (dir == NULL) {
+        perror("opendir");
         exit(EXIT_FAILURE);
     }
+    num_files = scandir(".", &namelist, NULL, alphasort);
 
-    while (n--) {
-        printf("%s\n", namelist[n]->d_name);
-        free(namelist[n]);
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
     }
-    free(namelist);
 
-    exit(EXIT_SUCCESS);
-}
-
-int readirr (){
-    DIR *dir;
-    struct dirent *dp;
-    char * file_name;
-    dir = opendir(".");
-    while ((dp=readdir(dir)) != NULL) {
-        printf("debug: %s\n", dp->d_name);
-        if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
-        {
-            // do nothing (straight logic)
-        } else {
-            file_name = dp->d_name; // use it
-            printf("file_name: \"%s\"\n",file_name);
-        }
-    }
     closedir(dir);
+    printf("Number of files: %d\n", num_files);
+
     return 0;
-}
-
-int opendirr () {
-    DIR *dir;
-    struct dirent *dp;
-    dir = opendir(".");   //what directory to open??
-    if (dir == NULL) {
-        perror("");
-    } else {
-        for (;;) {
-            dp = readdir(dir);
-            if ((dp = readdir(dir)) == NULL) break;
-            printf("%s\n", dp->d_name);
-
-            closedir(dir);
-        }
-    }
-}
-
-int main() {
-    scandirr();
-    printf("*******************************");
-    readirr();
-    printf("*******************************");
-    opendirr();
 }
