66d6d7d6735fe535e39950a14462facad07eee17
matvii0392 <tuo07428@temple.edu>
2024-02-01 13:53:46 -0500

    The program now prints only the high overview of the files when is asked ./file and prints only the overview of the directory contents and open folders when asked ./file folder

--- tuls.c
+++ tuls.c
@@ -33,8 +33,9 @@ void print_tree(const char *path, int depth) {
     while ((entry = readdir(dir)) != NULL) {
         if (entry->d_type == DT_DIR) {
             char newPath[1024];
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, ".." ) == 0 || strcmp(entry->d_name, ".DS_Store" ) == 0) {
                 continue;
+            }
             snprintf(newPath, sizeof(newPath), "%s/%s", path, entry->d_name);
             printf("%*s%s\n", depth, "", entry->d_name);
             print_tree(newPath, depth + 4);
@@ -54,65 +55,76 @@ void print_tree(const char *path, int depth) {
         struct dirent **namelist;
         char *positions[100];
         int iterations = 0;
-
-
-        //printf("commandline args: \n\n");
-        for (size_t i = 0; i < argc; i++) {
-            //printf("Position %li:%s\n", i, argv[i]);
-            positions[i] = argv[i];
-        }
-        //printf("-----------Current----------\n");
-        //printf("\nValue of argc (argument count) was %d\n", argc);
-
-
-        dir = opendir(".");
-        if (dir == NULL) {
-            perror("opendir");
-            exit(EXIT_FAILURE);
-        }
-
-
         char cwd[1024];
 
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            //printf("***Current directory: %s\n", cwd);
-        } else {
-            perror("getcwd() error");
-            return 1;
+        // variable to store size of Arr
+        //int length = sizeof(positions) / sizeof(positions[0]);
+
+        //printf("The length of the array is: %d\n", length);
+
+        printf("commandline args: \n\n");
+        for (size_t i = 0; i < argc; i++) {
+            printf("Position %li:%s\n", i, argv[i]);
+            positions[i] = argv[i];
         }
 
-        while ((entry = readdir(dir)) != NULL) {
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-                continue;
-            } // // Ignore "." and ".." entries
-            if (isDirectory(entry->d_name)) {
-                printf("> %s\n", entry->d_name);
+
+        if (positions[0] != NULL && positions[1] == NULL) {
+            dir = opendir(".");
+            if (dir == NULL) {
+                perror("opendir");
+                exit(EXIT_FAILURE);
             }
-            if (isFile(entry->d_name)) {
-                printf("%s\n", entry->d_name);
+            char cwd[1024];
+            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                //printf("***Current directory: %s\n", cwd);
+            } else {
+                perror("getcwd() error");
+                return 1;
             }
-        }
-
-        closedir(dir);
-
-        //printf("positions array:\n");
-        for (size_t j = 0; j < argc; j++) {
-            iterations++;
-            printf("%s\n", positions[j]);
-        }
-
-        if (iterations < 2) {
+            while ((entry = readdir(dir)) != NULL) {
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, ".." ) == 0 || strcmp(entry->d_name, ".cmake" ) == 0 ||
+                    strcmp(entry->d_name, ".DS_Store" ) == 0 || strcmp(entry->d_name, ".ninja_deps" ) == 0 || strcmp(entry->d_name, ".git" ) == 0 || strcmp(entry->d_name, ".idea" ) == 0) {
+                    continue;
+                } // // Ignore "." and ".." entries
+                if (isDirectory(entry->d_name)) {
+                    printf("> %s\n", entry->d_name);
+                }
+                if (isFile(entry->d_name)) {
+                    printf("%s\n", entry->d_name);
+                }
+            }
+            closedir(dir);
             exit(1); // Exit with error status 1
         } else {
-            printf("%s\n", "hello");
+            dir = opendir(".");
+            if (dir == NULL) {
+                perror("opendir");
+                exit(EXIT_FAILURE);
+            }
+            char cwd[1024];
+            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                //printf("***Current directory: %s\n", cwd);
+            } else {
+                perror("getcwd() error");
+                return 1;
+            }
+            while ((entry = readdir(dir)) != NULL) {
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, ".." ) == 0 || strcmp(entry->d_name, ".cmake" ) == 0 ||
+                    strcmp(entry->d_name, ".DS_Store" ) == 0 || strcmp(entry->d_name, ".ninja_deps" ) == 0 || strcmp(entry->d_name, ".git" ) == 0 || strcmp(entry->d_name, ".idea" ) == 0) {
+                    continue;
+                } // // Ignore "." and ".." entries
+                if (isDirectory(entry->d_name)) {
+                    //printf("> %s\n", entry->d_name);
+                }
+                if (isFile(entry->d_name)) {
+                    //printf("%s\n", entry->d_name);
+                }
+            }
+            closedir(dir);
+            printf("%s\n", "-------Searched directory--------");
             char str1[1024] = "/";
-            printf("%s\n", str1);
-            printf("%s\n", positions[1]);
             strcat(str1, positions[1]);
-            printf("United: %s\n", str1);
-            //printf("%s\n", strcat(str1,positions[1]));
-            //printf("%s\n", strcat(cwd, str1));
-            //dir1 = opendir(strcat(cwd, str1));   //works
             print_tree(strcat(cwd, str1), 0);
         }
     }
