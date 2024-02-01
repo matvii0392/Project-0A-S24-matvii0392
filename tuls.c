aa5b3c0b7c8553954ec44e003d3f4cfe1bbd6384
matvii0392 <tuo07428@temple.edu>
2024-01-31 23:59:18 -0500

    Trying out new tactir with dir1 (almost at setting up the loop)

--- tuls.c
+++ tuls.c
@@ -4,17 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
-
+#include <unistd.h>
+#include <string.h>
 int main(int argc, char *argv[]) {
     DIR *dir;
+    DIR *dir1;
     struct dirent *entry;
     int num_files;
     struct dirent **namelist;
+    char *positions[100];
+
 
     printf("commandline args: \n\n");
     for (size_t i = 0; i < argc; i++)
     {
         printf("Position %li:%s\n", i, argv[i]);
+        positions[i] = argv[i];
     }
     printf("-----------Current----------\n");
     //printf("\nValue of argc (argument count) was %d\n", argc);
@@ -26,6 +31,14 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     num_files = scandir(".", &namelist, NULL, alphasort);
+    char cwd[1024];
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("***Current directory: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+        return 1;
+    }
 
     while ((entry = readdir(dir)) != NULL) {
         printf("%s\n", entry->d_name);
@@ -33,7 +46,29 @@ int main(int argc, char *argv[]) {
 
 
     closedir(dir);
+
     printf("Number of files: %d\n", num_files);
+    printf("positions array:\n");
+    for (size_t j = 0; j < argc; j++){
+        printf("%s\n", positions[j]);
+    }
+
+
+    printf("__________________________________________\n");
+    //char cwd2[1024];strcat("cwd", positions[1]);
+    //printf("%s\n", strcat(cwd, strcat("/", positions[1])));
+    dir1 = opendir(strcat(cwd, strcat("/", positions[1])));
+    if (dir1 == NULL) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((entry = readdir(dir1)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+
+
+    closedir(dir1);
 
     return 0;
 }
