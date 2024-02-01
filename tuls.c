728fa1955615d44402a558efa82796eeeb6e820e
matvii0392 <tuo07428@temple.edu>
2024-02-01 02:57:24 -0500

    ...

--- tuls.c
+++ tuls.c
@@ -6,18 +6,30 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int isDirectory(const char *path) {
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISDIR(path_stat.st_mode);
+}
+
+
 int main(int argc, char *argv[]) {
     DIR *dir;
     DIR *dir1;
     struct dirent *entry;
     int num_files;
+    char *cwds[10000];
     struct dirent **namelist;
     char *positions[100];
+    int iterations = 0;
 
 
     printf("commandline args: \n\n");
-    for (size_t i = 0; i < argc; i++)
-    {
+    for (size_t i = 0; i < argc; i++) {
         printf("Position %li:%s\n", i, argv[i]);
         positions[i] = argv[i];
     }
@@ -30,6 +42,8 @@ int main(int argc, char *argv[]) {
         perror("opendir");
         exit(EXIT_FAILURE);
     }
+
+
     num_files = scandir(".", &namelist, NULL, alphasort);
     char cwd[1024];
 
@@ -41,42 +55,49 @@ int main(int argc, char *argv[]) {
     }
 
     while ((entry = readdir(dir)) != NULL) {
-        printf("%s\n", entry->d_name);
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        } // // Ignore "." and ".." entries
+        if (isDirectory(entry->d_name)) {
+            printf("> %s\n", entry->d_name);
+        }
     }
-
 
     closedir(dir);
 
-    printf("Number of files: %d\n", num_files);
     printf("positions array:\n");
-    for (size_t j = 0; j < argc; j++){
+    for (size_t j = 0; j < argc; j++) {
+        iterations++;
         printf("%s\n", positions[j]);
     }
 
+    if (iterations < 2) {
+        exit(1); // Exit with error status 1
+    } else {
+        printf("%s\n", "hello");
+        char str1[1024] = "/";
+        printf("%s\n", str1);
+        printf("%s\n", positions[1]);
+        strcat(str1,positions[1]);
+        printf("United: %s\n", str1);
+        //printf("%s\n", strcat(str1,positions[1]));
+        //printf("%s\n", strcat(cwd, str1));
+        dir1 = opendir(strcat(cwd, str1));   //works
+        if (dir1 == NULL) {
+            perror("opendir");
+            exit(EXIT_FAILURE);
+        }
 
-    printf("__________________________________________\n");
-    //char cwd2[1024] = "%s", strcat("cwd", positions[1]);
-    //printf("%s\n", strcat(cwd, "/testf"));
-    //printf("%s\n", strcat(cwd, strcat("/", positions[1])));
-    char str1[1024] = "/";
-    printf("%s\n", str1);
-    printf("%s\n", positions[1]);
-    strcat(str1,positions[1]);
-    printf("United: %s\n", str1);
-    //printf("%s\n", strcat(str1,positions[1]));
-    //printf("%s\n", strcat(cwd, str1));
-    dir1 = opendir(strcat(cwd, str1));   //works
-    if (dir1 == NULL) {
-        perror("opendir");
-        exit(EXIT_FAILURE);
+        while ((entry = readdir(dir1)) != NULL) {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+                continue;
+            }
+            printf("%s\n", entry->d_name);
+        }
+
+
+        closedir(dir1);
+
+        return 0;
     }
-
-    while ((entry = readdir(dir1)) != NULL) {
-        printf("%s\n", entry->d_name);
-    }
-
-
-    closedir(dir1);
-
-    return 0;
 }
